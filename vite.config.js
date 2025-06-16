3531d6f9f6ed566639bbbe1283a0162033b12297
ClarkWilliamsIE <clark@inertiaed.org>
2025-06-17 11:30:06 +1200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   root: '.', // Make sure Vite uses the root of the project as the base directory
   base: '/library-display/', // Adjust this if your GitHub Pages repository name is different
   build: {
     outDir: 'dist', // Output directory for the build
-    rollupOptions: {
-      input: '/public/index.html', // Explicitly set the input file to index.html
-    },
   },
 });
